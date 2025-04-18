ca43a36e9bc2f762ca071cf7e7691edd39a54efd
sofia <sofia.horlacher@hotmail.com>
2025-04-18 18:37:45 +0000

    Create dataset and torch lightning module

---
+++ data/dataset.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from PIL import Image
+import random
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+
+
+class UnpairedImageDataset(Dataset):
+    def __init__(
+        self,
+        domain_a_dir: str,
+        domain_b_dir: str,
+        transform: T.Compose = None,
+        image_extensions: tuple[str] = (".jpg", ".jpeg", ".png"),
+    ):
+        super().__init__()
+        self.domain_a_paths = self._load_paths(domain_a_dir, image_extensions)
+        self.domain_b_paths = self._load_paths(domain_b_dir, image_extensions)
+
+        self.transform = transform or T.Compose([
+            T.Resize((256, 256)),
+            T.ToTensor(),
+            T.Normalize((0.5,), (0.5,))
+        ])
+
+    def _load_paths(self, directory: str, exts) -> list[Path]:
+        return sorted([
+            p for p in Path(directory).glob("*") if p.suffix.lower() in exts
+        ])
+
+    def __len__(self):
+        return max(len(self.domain_a_paths), len(self.domain_b_paths))
+
+    def __getitem__(self, idx):
+        a_img_path = self.domain_a_paths[idx % len(self.domain_a_paths)]
+        b_img_path = random.choice(self.domain_b_paths)
+
+        a_img = Image.open(a_img_path).convert("RGB")
+        b_img = Image.open(b_img_path).convert("RGB")
+
+        return {
+            "a": self.transform(a_img),
+            "b": self.transform(b_img)
+        }
---
+++ tests/test_dataset.py
@@ -0,0 +1,31 @@
+def test_unpaired_dataset(tmp_path):
+    from data.dataset import UnpairedImageDataset
+    from PIL import Image
+    import numpy as np
+    domain_a = tmp_path / "domain_a"
+    domain_b = tmp_path / "domain_b"
+    domain_a.mkdir()
+    domain_b.mkdir()
+
+    img = Image.fromarray(np.uint8(np.random.rand(256, 256, 3) * 255))
+    img.save(domain_a / "a.jpg")
+    img.save(domain_b / "b.jpg")
+
+    dataset = UnpairedImageDataset(str(domain_a), str(domain_b))
+    sample = dataset[0]
+
+    assert sample["a"].shape == (3, 256, 256)
+    assert sample["b"].shape == (3, 256, 256)
+
+
+def test_datamodule_batch():
+    from data.datamodule import UnpairedDataModule
+    from pathlib import Path
+
+    path = Path("/home/jovyan/.cache/kagglehub/datasets/shubham1921/real-to-ghibli-image-dataset-5k-paired-images/versions/1/dataset")
+    dm = UnpairedDataModule(domain_a_dir=path / "trainA", domain_b_dir=path / "trainB_ghibli", batch_size=2, num_workers=0)
+    dm.setup()
+    batch = next(iter(dm.train_dataloader()))
+
+    assert batch["a"].shape == (2, 3, 256, 256)
+    assert batch["b"].shape == (2, 3, 256, 256)
