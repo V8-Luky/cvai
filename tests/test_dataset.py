289f02d2e9a8ab925c40877d846dd9cbc86bc544
sofia <sofia.horlacher@hotmail.com>
2025-04-19 16:24:11 +0000

    added unit test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -20,6 +20,21 @@ def test_unpaired_dataset(tmp_path):
     assert sample["a"].shape == (3, 256, 256)
     assert sample["b"].shape == (3, 256, 256)
 
+def test_unpaired_dataset_larger_images(tmp_path):
+    domain_a = tmp_path / "domain_a"
+    domain_b = tmp_path / "domain_b"
+    domain_a.mkdir()
+    domain_b.mkdir()
+
+    img = Image.fromarray(np.uint8(np.random.rand(512, 512, 3) * 255))
+    img.save(domain_a / "a.jpg")
+    img.save(domain_b / "b.jpg")
+
+    dataset = UnpairedImageDataset(str(domain_a), str(domain_b))
+    sample = dataset[0]
+
+    assert sample["a"].shape == (3, 256, 256)
+    assert sample["b"].shape == (3, 256, 256)
 
 def test_datamodule_batch():
     path = Path("/home/jovyan/.cache/kagglehub/datasets/shubham1921/real-to-ghibli-image-dataset-5k-paired-images/versions/1/dataset")
